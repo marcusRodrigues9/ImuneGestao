5cc33f7f396617351f861ce337cb105656aad95a
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2025-07-16 20:30:17 -0300

    criação da tela de agendamentos, funcionalidades OK, alteração de telas/navegação de uma tela para outra

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module com.imunegestao.imunegestao {
+    requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+
 
     opens com.imunegestao to javafx.fxml;
     opens com.imunegestao.controllers to javafx.fxml;
